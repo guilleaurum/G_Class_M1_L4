6d6f4566d905dc320da5b46f2b54e73eb68a5db7
guilleaurum <guilleaurum@gmail.com>
2026-01-03 16:57:45 +0100

    Subida de archivos
    bot.py y bot_logic.py

---
+++ bot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hemos iniciado sesión como {bot.user}')
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send(f'Hola, soy un bot {bot.user}!')
+
+@bot.command()
+async def contraseña(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def gato(ctx):
+    await ctx.send('Adoro los gatos')
+    await ctx.send('Me encantan los gatos')
+
+@bot.command()
+async def repite(ctx, times: int, content='Gatos'):
+    """Repeats a message multiple times."""
+    for i in range(times):
+        await ctx.send(content)
+
+@bot.command()
+async def perro(ctx):
+    await ctx.send('Solo quiero a los gatos')
+
+bot.run("MTQ1MTk1MDY1NDU2NTM4ODMxMQ.GdyPF6.y57aECeCcwwlkE0KzLCCR3sOU6HaErv0Z9jCKE")
